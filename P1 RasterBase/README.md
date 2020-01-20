f2e17f39597a5a881dfb35a4f1760c28f9c46ab8
Chenxi Xiao <chxiao@calpoly.edu>
2020-01-19 19:10:47 -0800

    finished!

--- P1 RasterBase/README.md
+++ P1 RasterBase/README.md
@@ -1,6 +1,10 @@
 Name: Cici Xiao
 
 Information about the distance coloring point
+Red: 255 * (1- abs(sin(500*z)))
+Blue:255 * abs(sin(50*z))
+Green: 255 * abs(sin(500*z))
+
 Citations for any downloaded code 
 1. Dr. Philippe B. Laval. Mathematics for Computer Graphics - Barycentric Coordinates. From https://polylearn.calpoly.edu/AY_2019-2020/pluginfile.php/573699/mod_resource/content/6/barycentric.pdf
 2. Syoyo Fujita. Obj Loader. From https://github.com/syoyo/tinyobjloader
--- P1 RasterBase/src/main.cpp
+++ P1 RasterBase/src/main.cpp
@@ -14,8 +14,10 @@
 // You should never do this in a header file.
 using namespace std;
 //global
-int g_width, g_height;
+int g_width, g_height, option;
 float L, R, B, T, C, D, E, F;
+float alpha, beta, gama;
+double frequency = 500.0,freq = 5.0;
 
 /*
    Helper function you will want all quarter
@@ -82,64 +84,74 @@ void resize_obj(std::vector<tinyobj::shape_t> &shapes){
       }
    }
 }
-//----------MY HELPER FUNCTIONS----------------//
-double calcArea(vector<float> A, vector<float> B, vector<float> C){
-    double area = ((A[0] * (B[1] - C[1]) + B[0] * (C[1] - A[1]) + C[0] * (A[1] - B[1])) / 2);
+
+//calculate area of a 2D triangle
+float calcArea(vector<float> A, vector<float> B, vector<float> C){
+    float area = (A[0] * (B[1] - C[1]) + B[0] * (C[1] - A[1]) + C[0] * (A[1] - B[1])) / 2;
     return abs(area);
 }
-int isinside(vector<float> A, vector<float> B, vector<float> C, vector<float> pt, double* alpha, double* beta, double* gamma)
-{//get three vertices in pixel world <int> and return the area
+
+//test if pt is inside of a triangle
+int isinside(vector<float> A, vector<float> B, vector<float> C, vector<float> pt)
+{
     float epsilon = 0.001;
-    double total = calcArea(A, B, C);
-    double a1 = calcArea(A, B, pt);
-    double a2 = calcArea(B, C, pt);
-    double a3 = calcArea(C, A, pt);
-    *alpha = a1/total;
-    *beta = a2/total;
-    *gamma = a3/total;
-    //cout<< "ALPHA" << *alpha<< endl;
-    if (*alpha > epsilon|| *alpha < (1+epsilon) || *beta > epsilon || *beta < (1+epsilon) || *gamma > epsilon || *gamma < (1+epsilon)){//α+β+γ=1 or 0<α/β/γ<1 inside. Add epsilon to account for floating point tomfoolery.
+    float total = calcArea(A, B, C);
+    float a1 = calcArea(pt,B, C);  //Aa
+    float a2 = calcArea(pt, C, A); //Ab
+    float a3 = calcArea(pt, A, B); //Ac
+    alpha = a1/total;
+    beta = a2/total;
+    gama = a3/total;
+    
+    //Add epsilon to account for floating point tomfoolery.
+    if(abs(a1+a2+a3 - total) <= 1.0-epsilon){
         return 1;
     }
     return 0;
 }
 
-vector<float> W2P(vector<float> V){ //X-axis: xp = C * xw + D //Y-axis: ; yp = E * yw + D;
+//convert world points into pixel points
+//X-axis: xp = C * xw + D; Y-axis: yp = E * yw + D;
+vector<float> W2P(vector<float> V){
     float pixel_x = C * V[0] + D;
     float pixel_y = E * V[1] + F;
-    return {pixel_x, pixel_y, V[2]};
+    return {pixel_x, pixel_y};
 }
 
-void Print(vector<float> V){
-    cout<< "Vertices: "<< V[0] <<" "<< V[1] << " " << V[2]<< endl;
+//set up z buffer and fill it with a fixed value to ensure depth testing isn't done with garbage data.
+float* SetUpZbuffer(int g_width, int g_height){
+    float* zbuffer;
+    zbuffer = new float[g_width* g_height];
+    for(int y = 0; y < g_height; ++y){
+        for(int x = 0; x < g_width; ++x){
+            zbuffer[y*g_width + x] = -200.0;
+        }
+    }
+    return zbuffer;
 }
 
-int main1(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if(argc < 3) {
-      //cout << "Usage: raster meshfile imagefile" << endl;
+	if(argc < 6) {
       cout << "Usage: raster meshfile imagefile width height coloring_mode" << endl;
       return 0;
    }
-    for(int i = 0; i < argc; i++){
-        cout<< argv[i] <<" ";
-    }
 	// OBJ filename
 	string meshName(argv[1]);
 	string imgName(argv[2]);
-
-	//set g_width and g_height appropriately!
+    
     g_width = stoi(argv[3]);
     g_height = stoi(argv[4]);
+    option = stoi(argv[5]);
+    
    //create an image
 	auto image = make_shared<Image>(g_width, g_height);
-
+    //create a 1D array Z buffer for depth testing
+    float* zbuffer = SetUpZbuffer(g_width, g_height);
 	// triangle buffer
 	vector<unsigned int> triBuf;
 	// position buffer
 	vector<float> posBuf;
-	// Some obj files contain material information.
-	// We'll ignore them for this assignment.
 	vector<tinyobj::shape_t> shapes; // geometry
 	vector<tinyobj::material_t> objMaterials; // material
 	string errStr;
@@ -154,36 +166,26 @@ int main1(int argc, char **argv)
 		posBuf = shapes[0].mesh.positions;
 		triBuf = shapes[0].mesh.indices;
 	}
-	cout << "Number of vertices: " << posBuf.size()/3 << endl;
-	cout << "Number of triangles: " << triBuf.size()/3 << endl;
     
     //get L R B T C D E F
     if(g_width > g_height){
-        L = -1*(g_width/g_height);
-        R = g_width/g_height;
-        B = -1;
-        T = 1;
+        R = (float)g_width/(float)g_height;
+        L = -1.0*R;
+        B = -1.0;
+        T = 1.0;
+    }else{
+        T = (float)g_height/(float)g_width;
+        B = -1.0*T;
+        L = -1.0;
+        R = 1.0;
     }
-    else{
-        L = -1;
-        R = 1;
-        B = -1*(g_height/g_width);
-        T = g_height/g_width;
-    }
-    // 0 = C * L + D  ; width - 1 = C * R + D
-    // (width - 1) - 0 = C(R-L) --> C = (width-1)/(R-L)
-    //put C into 1st equation: D = (1-width)*L/(R-L)
-    C = (g_width-1) / (R-L);
-    D = (1-g_width) * L / (R-L);
-    E = (g_height - 1) / (T-B);
-    F = (1-g_height) * B/ (T-B);
-    
+    // 0 = C * L + D  ; width - 1 = C * R + D ; (width - 1) - 0 = C(R-L)
+    C = (g_width-1.0) / (R-L);
+    D = -1.0*C*L;
+    E = (g_height - 1.0) / (T-B);
+    F = -1.0*E*B;
+
     //convert world points xw <float> into pixel points xp <int>
-    
-    cout << "C D E F: "<< C << " " << D << " " << E << " "<< F << endl;
-    cout << "triBuf size " << triBuf.size() << endl;
-	//TODO add code to iterate through each triangle and rasterize it 
-   //shapes[i].mesh.positions stores the vertices as {x0,y0,z0... x,y,z...}
     for (size_t i = 0; i < triBuf.size()/3; i++) {
         int Xmax, Xmin, Ymax, Ymin;
         // get vertices
@@ -194,39 +196,53 @@ int main1(int argc, char **argv)
         vector<float> V1 {posBuf[3*i1+0], posBuf[3*i1+1], posBuf[3*i1+2]};
         vector<float> V2 {posBuf[3*i2+0], posBuf[3*i2+1], posBuf[3*i2+2]};
         vector<float> V3 {posBuf[3*i3+0], posBuf[3*i3+1], posBuf[3*i3+2]};
-        //         float xw1 = posBuf[3*V1+0] ;
-        //         float yw1 = posBuf[3*V1+1] ;
-        //         float xw2 = posBuf[3*V2+0];
-        //         float yw2 = posBuf[3*V2+1];
-        //         float xw3 =posBuf[3*V3+0];
-        //         float yw3 =posBuf[3*V3+1];
-
-        //convert {Xw, Yw} -> {Xp,Yp,Zw}   vector{x,y,z}
-        //W2P updates only XY
+        
+        //convert {Xw, Yw} -> {Xp,Yp}
         vector<float> V1p = W2P(V1);
         vector<float> V2p = W2P(V2);
         vector<float> V3p = W2P(V3);
 
-        Xmax = max(max(V1p[0], V2p[0]),V3p[0]);
-        Xmin = min(min(V1p[0], V2p[0]),V3p[0]);
-        Ymax = max(max(V1p[1], V2p[1]),V3p[1]);
-        Ymin = min(min(V1p[1], V2p[1]),V3p[1]);
+        Xmax = (int)max(max(V1p[0], V2p[0]),V3p[0]);
+        Xmin = (int)min(min(V1p[0], V2p[0]),V3p[0]);
+        Ymax = (int)max(max(V1p[1], V2p[1]),V3p[1]);
+        Ymin = (int)min(min(V1p[1], V2p[1]),V3p[1]);
         
         //compute bounding box
         for (int y = Ymin; y <= Ymax; y++) {
             for (int x = Xmin; x <= Xmax; x++) {//for each pixel in the bounding box
                 vector<float>pt {(float)x, (float)y};
-                double alpha = 0, beta = 0, gamma =0;
-                if (isinside(V1p, V2p, V3p, pt, &alpha, &beta, &gamma)==1){//α+β+γ=1 --> inside
-                    //update z
-                    double z = alpha * V1p[2]+ beta * V2p[2] + gamma * V3p[2];
-                    image->setPixel(x, y, 255, 255, 0);
-                    //occlusion : some triangles might not be visible due to z order
-                    //Start Coloring
-                    //1: map the z-value to the range 0 to 255.
-                    //2. Z- buffer
-                    
-                   
+                if (isinside(V1p, V2p, V3p, pt)){
+                    /*use alpha beta gamma to get Z */
+                    /* -1 <=  z  <= 1
+                     0 <= z+1 <= 2
+                    0 <= 127.5 * (z+1) <= 255*/
+                    float z = alpha * (V1[2]) + beta * (V2[2]) + gama * (V3[2]) + 1;
+        //--------Start Coloring 1: map the z-value to the range 0 to 255. 2. Z- buffer-----
+        // If its more (closer) cur z is smaller, do not overwrite the pixel or z value.closer vertices are brighter
+                    int r,b,g;
+                    //standard shading
+                    if(option == 1){
+                       if(zbuffer[y*g_width + x] < z){
+                           zbuffer[y*g_width + x] = z;
+                           r = zbuffer[y*g_width + x] * (255/2);
+                           b = 0;
+                           g = 0;
+                           image->setPixel(x, y, r, g, b);
+                        }
+                    }
+                    //use sin to mix colors
+                    else if (option == 2){
+                        r = 255 - 255*abs(sin(frequency*z));
+                        b = 255 * abs(sin(freq*z)) ;
+                        g = 255 * abs(sin(frequency*z));
+                        image->setPixel(x, y, r, g, b);
+                    }
+                    else {
+                        /* error checking on coloring mode */
+                        cout << "Coloring mode " << option << " is not valid" << endl;
+                        cout << "mode 1: shading | mode 2: distance coloring" << endl;
+                        return 0;
+                    }
                 }
             }
         }
@@ -238,25 +254,3 @@ int main1(int argc, char **argv)
 }
 
 
-int main(int argc, char *tf[]){
-     tf[1] =  "/Users/ccmac/Documents/GitHub/CPE471/P1 RasterBase/resources/bunny.obj";
-    tf[2] = "bunny_bbox.png";
-     tf[3] = "500";
-     tf[4] = "500";
-    main1(6, tf);
-     tf[1] =  "/Users/ccmac/Documents/GitHub/CPE471/P1 RasterBase/resources/sphere.obj";
-     tf[2] = "sphere_bbox.png";
-    main1(6, tf);
-    tf[1] =  "/Users/ccmac/Documents/GitHub/CPE471/P1 RasterBase/resources/teapot.obj";
-    tf[2] = "teapot__box.png";
-    main1(6, tf);
-    tf[1] =  "/Users/ccmac/Documents/GitHub/CPE471/P1 RasterBase/resources/tri.obj";
-    tf[2] = "tri__box.png";
-    main1(6, tf);
-    tf[3] = "500";
-    tf[4] = "100";
-    tf[1] =  "/Users/ccmac/Documents/GitHub/CPE471/P1 RasterBase/resources/teapot.obj";
-    tf[2] = "teapot__ratio.png";
-    main1(6, tf);
-    
-}
